8891e5294648005db0b226d385dbe55c2bbdfdb6
Ntwari Moise <mozayntwali@gmail.com>
2021-04-28 09:59:36 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <message>Hello there</message>
+    <message>Doing good</message>
+  </div>
+</template>
+
+<script>
+import Message from './components/Message'
+export default {
+  name: 'app',
+  components: { Message },
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
